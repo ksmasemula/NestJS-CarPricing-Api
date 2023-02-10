982ef2981321686e0bc0c7db609afa7bf339f27e
Kabelo Masemula <ksmasemula@gmail.com>
2023-02-11 00:48:41 +0200

    added route setup and service create user function

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,8 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
-import { ConfigModule } from '@nestjs/config';
-import configuration from './config/configuration';
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      load: [configuration],
-    }),
     TypeOrmModule.forRoot({
     type: 'sqlite',
     database: 'db.sqlite',
